70130b600e0641f84eba2fee8e4a605c66ab3353
camilo cano <canojuancamilo99@gmail.com>
2023-11-03 14:32:57 -0500

    se agregan los modelos

---
+++ CarritoCompra/Models/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using BCrypt.Net;
+
+namespace CarritoCompra.Models
+{
+    public class Pedido
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id_pedido { get; set; }
+
+        public int id_producto { get; set; }
+        public Producto Producto { get; set; }
+
+        public int cantidad { get; set; }
+
+        public DateTime fecha_pedido { get; set; }
+    }
+}
---
+++ CarritoCompra/Models/Transaccion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using BCrypt.Net;
+
+namespace CarritoCompra.Models
+{
+    public class Transaccion
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id_transaccion { get; set; }
+        public int id_usuario { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime fecha_transaccion { get; set; }
+    }
+}

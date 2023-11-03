bd143261f52b4050fadb1281c769f6d9b4799e05
camilo cano <canojuancamilo99@gmail.com>
2023-11-03 12:41:51 -0500

    Se crea el proyecto

---
+++ CarritoCompra/Models/Perfil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarritoCompra.Models
+{
+    public class Perfil
+    {
+        public int id_perfil { get; set; }
+    }
+}

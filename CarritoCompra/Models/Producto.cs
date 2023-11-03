0f08485ccd49f5c0a8fa32e776dea519bb5d2e0a
camilo cano <canojuancamilo99@gmail.com>
2023-11-03 14:15:24 -0500

    Se agrega el producto a los modelos

---
+++ CarritoCompra/Models/Producto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using BCrypt.Net;
+
+namespace CarritoCompra.Models
+{
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id_producto { get; set; }
+
+        public int id_categoria { get; set; }
+
+        public CategoriaProducto CategoriaProducto { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido.")]
+        [StringLength(150, ErrorMessage = "No puede tener más de 150 caracteres.")]
+        public string nombre { get; set; }
+        
+        [Required(ErrorMessage = "El campo es requerido.")]
+        public int CantidadDisponible { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido.")]
+        [StringLength(200, ErrorMessage = "No puede tener más de 200 caracteres.")]
+        public string Descripcion { get; set; }
+    }
+}
